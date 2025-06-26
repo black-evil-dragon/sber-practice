790090928616612486aca7b6606f19981a162169
Semyon Golgan <blackevildragon@mail.ru>
2025-06-26 16:44:03 +0300

    [dev][unit2] Создание класса Circle
    Создайте класс Circle, который содержит переменные radius, color;
    методы area() и perimeter(), которые возвращают значение площади и периметра соответственно.
    Добавьте конструктор - Circle(radius, color) .
    Реализовать метод toString(), который выведет информацию о круге (радиус, цвет, площадь, периметр)
    Создать класс с методом main, в котором продемонстрировать работу класса.

--- units/unit2/src/main/java/ru/sbertech/practice/Main.java
+++ units/unit2/src/main/java/ru/sbertech/practice/Main.java
@@ -47,18 +47,57 @@ class Phone {
 }
 
 
+// Task 2 - Circle
+class Circle {
+    private double radius;
+    private double perimeter;
+    private double area;
+
+    private String color;
+
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+        this.perimeter = perimeter();
+        this.area = area();
+
+    }
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle(" + radius + ", " + color + ", area:" + this.area + ", length:" + this.perimeter + ')';
+    }
+}
+
+
 
 public class Main {
     public static void main(String[] args) {
         Phone phone = new Phone("+79114493901", "samsung", "150");
 
         System.out.println("Телефон: " + phone);
-        
+
         phone.receiveCall("Иван Иванов");
         phone.receiveCall("МОШЕННИК", "+74901551515");
 
         System.out.println("Номер телефона: " + phone.getNumber());
 
         phone.sendMessage("+79114493901");
+
+
+        Circle circle1 = new Circle(5.0, "Красный");
+        Circle circle2 = new Circle(3.5, "Синий");
+
+        System.out.println(circle1);
+        System.out.println(circle2);
     }
 }
