b992f6d2be8f19ddd33e2568cfd8d5c2a0310627
Semyon Golgan <blackevildragon@mail.ru>
2025-06-26 15:47:10 +0300

    [dev][unit1] Создание обработчика выбора задания

--- units/unit1/src/main/java/ru/sbertech/practice/Main.java
+++ units/unit1/src/main/java/ru/sbertech/practice/Main.java
@@ -1,10 +1,13 @@
 package ru.sbertech.practice;
 
+import java.util.Scanner;
+
 public class Main {
     static void sayHello() {
         System.out.println("Hello World!");
     }
 
+    
     static void divide(int a, int b) {
         if (b == 0) {
             System.out.println("Деление на ноль");
@@ -14,6 +17,7 @@ public class Main {
             System.out.println(a + " / " + b + " = " + r + " и " + c + " в остатке");
         }
     }
+
 
     static void isPalindrom(String string) {
         if (string.length() == 1) {
@@ -29,43 +33,61 @@ public class Main {
         System.out.println("Строка является палиндромом: " + string);
     }
 
-    static void summator(int a) {
-        // String string = "" + a; // well-well-well
-//        String string = Integer.toString(a);
-//
-//        int sum = 0;
-//        for (int i = 0; i < string.length(); i++) {
-//
-//        }
 
-        // Трехзначное число
+    static void summator(int a) {
         System.out.println(a % 10 + (a / 10) % 10 + a / 100);
 
     }
+
 
     static void moonfulMass(int mass) {
         System.out.println(mass * 0.17);
     }
 
+
     public static void main(String[] args) {
-        // Task 1
-        sayHello();
 
-        // Task 2
-        divide(10, 3);
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
 
-        // Task 3
-        isPalindrom("abba");
+        switch (choice) {
+            case 1:
+                sayHello();
+                break;
 
-        // Task 4
-        summator(134);
+            case 2:
+                System.out.println("Введите два числа:");
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+                divide(a, b);
+                break;
 
-        // Task 5
-        moonfulMass(60);
+            case 3:
+                System.out.println("Введите строку для проверки:");
+                scanner.nextLine();
+                String str = scanner.nextLine();
+                isPalindrom(str);
+                break;
+
+            case 4:
+                System.out.println("Введите трехзначное число:");
+                int num = scanner.nextInt();
+                summator(num);
+                break;
+
+            case 5:
+                System.out.println("Введите массу на Земле:");
+                int mass = scanner.nextInt();
+                moonfulMass(mass);
+                break;
+
+            default:
+                System.out.println("Неверный выбор. Попробуйте снова.");
+        }
+
+        scanner.close();
+
 
 
     }
-
-
-
 }
