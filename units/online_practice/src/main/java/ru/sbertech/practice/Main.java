e8ba1fa5da2ae4b36c8340413904143116b223b0
Semyon Golgan <94925577+black-evil-dragon@users.noreply.github.com>
2025-06-27 19:10:29 +0300

    [draft][online] Практика 27.06
    Экспепшены
    Классы
    record
    isVar (bool) - getVar (another)
    Мир состоит из object
    Дженерики

--- units/online_practice/src/main/java/ru/sbertech/practice/Main.java
+++ units/online_practice/src/main/java/ru/sbertech/practice/Main.java
@@ -1,37 +1,11 @@
 package ru.sbertech.practice;
 
 
-class Test {
-    private int id;
-
-    public Test() {
-        this.id = 10;
-    }
-
-    public void print() {
-        System.out.println("Hello");
-    }
-
-
-    public int getId() {
-        return id;
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        Test test = new Test() {
-            @Override
-            public void print() {
-                System.out.println("Hello world!");
-            }
-        };
+        String string = "Hello world";
+        String another_string = new String("Eps");
 
-        test.print();
-        if ("1".equals(1)) {
-            System.out.println("Hello");
-        }
-
-//        (1).equals(1);
+        string.equals(another_string);
     }
 }
